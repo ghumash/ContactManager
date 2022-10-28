cb048d4aa72c0c243e801844d42bf15d401cdd4f
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2022-10-28 09:27:23 +0400

    Refactor: "Header.css" file

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
 
 export default function Header() {
-  return <div className="Header">Contact Manager</div>;
+  return <div className="Header-container">Contact Manager</div>;
 }
