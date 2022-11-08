68312746a27f98db5e4342425727366debf8e0ac
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2022-11-08 20:33:31 +0400

    Add: css folder

--- src/List/ListItem/ListItem.jsx
+++ src/List/ListItem/ListItem.jsx
@@ -2,6 +2,7 @@ import "./ListItem.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function ListItem({
   id,
@@ -18,19 +19,51 @@ export default function ListItem({
   onEdit,
   onCheck,
 }) {
-  const ListItemStyleHandler = () => {
-    if (cardViewState) {
-      return "ListItem-card";
-    } else {
-      if (checkedIdArr.includes(id)) {
-        return "ListItem-row itemRowInlineStyle";
-      } else {
-        return "ListItem-row";
-      }
-    }
-  };
+  const [inlineItemState, setInlineItemState] = useState(null);
+  const [inlineItemEditState, setInlineItemEditState] = useState(true);
 
-  const inlineItem = (
+  const [firstNameInput, setFirstNameInput] = useState(firstName);
+  const [lastNameInput, setLastNameInput] = useState(lastName);
+  const [phoneInput, setPhoneInput] = useState(phone);
+  const [emailInput, setEmailInput] = useState(email);
+  const [professionInput, setProfessionInput] = useState(profession);
+
+  const inlineItemEdit = (
+    <>
+      <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
+        {avatar}
+        <input
+          type="text"
+          value={firstNameInput}
+          onChange={(e) => {
+            console.log(e.target.value)
+            setFirstNameInput(e.target.value);
+          }}
+        />
+        {/* <input type="text" value={lastName} /> */}
+      </div>
+      <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
+        {cardViewState ? (
+          <p className="ListItem-carView-subtitle">Email</p>
+        ) : null}
+        <a href={`mailto:${{ email }}`}>{email}</a>
+      </div>
+      <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
+        {cardViewState ? (
+          <p className="ListItem-carView-subtitle">Phone</p>
+        ) : null}
+        <a href={`tel:${{ phone }}`}>{phone}</a>
+      </div>
+      <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
+        {cardViewState ? (
+          <p className="ListItem-carView-subtitle">Profession</p>
+        ) : null}
+        {profession}
+      </div>
+    </>
+  );
+
+  const inlineItemView = (
     <>
       <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
         {avatar}
@@ -57,7 +90,27 @@ export default function ListItem({
     </>
   );
 
-  const inlineEditHandler = () => {};
+  const ListItemStyleHandler = () => {
+    if (cardViewState) {
+      return "ListItem-card";
+    } else {
+      if (checkedIdArr.includes(id)) {
+        return "ListItem-row itemRowInlineStyle";
+      } else {
+        return "ListItem-row";
+      }
+    }
+  };
+
+  const inlineEditHandler = () => {
+    if (inlineItemEditState) {
+      setInlineItemState(inlineItemEdit);
+      setInlineItemEditState(false);
+    } else {
+      setInlineItemState(inlineItemView);
+      setInlineItemEditState(true);
+    }
+  };
 
   return (
     <div className={ListItemStyleHandler()}>
@@ -74,7 +127,9 @@ export default function ListItem({
           <span></span>
         </label>
       </div>
-      {inlineItem}
+
+      {inlineItemState ? inlineItemState : inlineItemView}
+
       <div className={!cardViewState ? "ListItem-item" : "ListItem-card-item"}>
         <div className="ListItem-icon-group">
           <FontAwesomeIcon
