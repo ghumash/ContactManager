bf2725133099acd6faa15b77744e23724a08c979
ghumash <tigranghumashyan@gmail.com>
2022-12-06 19:01:53 +0400

    Add: context for App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./app/App";
+import ThemeContext from "./context/SettingsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <App/>
-  </HashRouter>
+  <ThemeContext.Provider>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ThemeContext.Provider>
 );
